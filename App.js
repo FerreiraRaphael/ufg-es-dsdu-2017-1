600b1731c4e262f2c466d5009edb2b20d2956808
Raphael Ferreira <raphaelbsferreira@gmail.com>
2017-06-05 01:34:26 -0300

    commit inicial

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import MapScene from './scenes/MapScene/MapScene'
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      'Ionicons': require('native-base/Fonts/Ionicons.ttf'),
+    });
+
+    this.setState({isReady: true});
+  }
+
+  render() {
+     if (!this.state.isReady) {
+      return <Expo.AppLoading />;
+    }
+    
+    return (
+      <MapScene/>
+    );
+  }
+}
