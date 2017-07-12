cc3beb1b8434becf53df2675f771ccb775e2aa4d
joao <joao_camargo10@hotmail.com>
2017-07-12 20:11:58 -0300

    ajuste na busca de endereços do google

--- services/maps.js
+++ services/maps.js
@@ -3,7 +3,7 @@ import config from "../config/config";
 const apiUrl = "https://maps.googleapis.com/maps/api/geocode/json";
 
 export const getAddress = async ({ latitude, longitude, latitudeDelta, longitudeDelta }) => {
-  let url = `${apiUrl}?latlng=${latitude},${longitude},${latitudeDelta},${longitudeDelta}&key=${config.geolocationKey}`;
+  let url = `${apiUrl}?latlng=${latitude},${longitude}&key=${config.geolocationKey}`;
   let result = await fetch(url).then(res => res.json());
   return result;
 };
